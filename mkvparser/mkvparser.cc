562301346008c30d4a9bfc4e516d675320d4a55a
Frank Galligan <fgalligan@google.com>
2019-05-03 16:13:45 -0700

    Fixes a double free in ContentEncoding
    BUG=b/127702368
    
    Change-Id: Ifa958d72f8e2e75bae4cddd5c6d3625882da7c2b

--- mkvparser/mkvparser.cc
+++ mkvparser/mkvparser.cc
@@ -4230,6 +4230,7 @@ long ContentEncoding::ParseContentEncodingEntry(long long start, long long size,
         new (std::nothrow) ContentEncryption*[encryption_count];
     if (!encryption_entries_) {
       delete[] compression_entries_;
+      compression_entries_ = NULL;
       return -1;
     }
     encryption_entries_end_ = encryption_entries_;
