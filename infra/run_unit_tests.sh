a8283f4f784c903f740b30884d4f6221550071da
Roberto Alanis <alanisbaez@google.com>
2021-12-15 12:43:32 +0000

    Fix asan_symbolize execution by adding pipe
    Change-Id: I5845819286a587670c64e4e9314390d0f52da0e5
    Bug: b:185520494

--- infra/run_unit_tests.sh
+++ infra/run_unit_tests.sh
@@ -103,8 +103,8 @@ dump_sanitizer_log() {
     *-asan)
       asanlog_symbolized="${BUILD_DIR}/asan_log.asanlog_symbolized"
       grep -v 'Invalid VP9' "${SANITIZER_LOG}" > "${SANITIZER_LOG}.2" || true
-      "${asan_symbolize_tool}" "${BUILD_DIR}" c++filt < "${SANITIZER_LOG}.2" \
-        > "${asanlog_symbolized}"
+      "${asan_symbolize_tool}" "${BUILD_DIR}" < "${SANITIZER_LOG}.2" \
+        | c++filt > "${asanlog_symbolized}"
       if [[ -s "${asanlog_symbolized}" ]]; then
         cat "${asanlog_symbolized}"
         return 1
